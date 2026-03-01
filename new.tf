8221ced0f94fee05172173b24dfc5e45d5d25c40
Avinash <plazy9626@gmail.com>
2026-03-01 23:04:39 +0530

    second

--- new.tf
+++ new.tf
@@ -4,12 +4,20 @@ provider "aws" {
     secret_key = "beuiyrSOI3yqYSFYriH9e4CT/rJktyqCzsEAwviz"
   
 }
+# resource "aws_instance" "example" {
+#   for_each = toset(["first", "second","third","fourth"])
+#   ami           = "ami-051a31ab2f4d498f5"
+#   instance_type = "t3.micro"
+#   tags = {
+#     Name = each.key
+#       }
+# }
 resource "aws_instance" "example" {
-  for_each = toset(["first", "second","third","fourth"])
+  
   ami           = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
   count = 4
   tags = {
-    Name = "HelloWorld"
-  }
+    Name = webserver${index.count}
+      }
 }
