90becb3c93d85bd6e578f894d5c1ba0898692a92
Avinash <plazy9626@gmail.com>
2026-03-01 23:07:19 +0530

    third

--- new.tf
+++ new.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-    region = "ap-south-1"
-    access_key = "AKIARDVMPTPEPJDXLYLB"
-    secret_key = "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"
+# provider "aws" {
+    # region = "ap-south-1"
+    # access_key = ""
+    # secret_key = ""
   
-}
+# }
 # resource "aws_instance" "example" {
 #   for_each = toset(["first", "second","third","fourth"])
 #   ami           = "ami-051a31ab2f4d498f5"
@@ -18,6 +18,6 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   count = 4
   tags = {
-    Name = webserver${index.count}
+    Name = "webserver${index.count}"
       }
 }
