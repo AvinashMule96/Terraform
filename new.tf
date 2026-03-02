e4ed2cb5f9225807a09e609e19842c8566a62b7c
Avinash <plazy9626@gmail.com>
2026-03-03 01:33:34 +0530

    added instance with sg

--- new.tf
+++ new.tf
@@ -11,12 +11,34 @@
 #     Name = each.key
 #       }
 # }
-resource "aws_instance" "example" {
+# resource "aws_instance" "example" {
   
-  ami           = "ami-051a31ab2f4d498f5"
-  instance_type = "t3.micro"
-  count = 4
-  tags = {
-    Name = "webserver${index.count}"
-      }
-}
+  # ami           = "ami-051a31ab2f4d498f5"
+  # instance_type = "t3.micro"
+  # count = 4
+  # tags = {
+    # Name = "webserver${index.count}"
+      # }
+# }
+
+
+
+
+
+
+
+provider "aws" {
+  region = "ap-south-1"
+
+  }
+
+  resource "aws_instance" "terra" {
+    ami = "ami-051a31ab2f4d498f5"
+    instance_type = "t3.micro"
+    key_name = "my-key"
+    vpc_security_group_ids = "sg-04066b02d89d6dc20"
+    tags = {
+      Name = "new"
+    }
+    
+  }
